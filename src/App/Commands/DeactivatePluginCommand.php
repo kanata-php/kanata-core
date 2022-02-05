9171700cdd4e6feb03ae669c66d05ce6f32d2a79
Savio Resende <savio@savioresende.com.br>
2022-02-04 19:32:33 -0700

    wip

--- src/App/Commands/DeactivatePluginCommand.php
+++ src/App/Commands/DeactivatePluginCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class ActivatePluginCommand extends Command
+class DeactivatePluginCommand extends Command
 {
     use LogoTrait;
 
